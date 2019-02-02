5f69629de91598f4607d8ee944fc83e1070b0b99
Atick Faisal <atick.faisal@ieee.org>
2019-02-02 18:56:28 +0600

    initial commit

---
+++ Timer/README.md
@@ -0,0 +1,2 @@
+# PIC16F877a
+pic16f877a mini projects using mplab x ide and xc8 compiler 
---
+++ Timer/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=timer_interrupt.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/timer_interrupt.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=timerinterrupt.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/timerinterrupt.x.tar
