40c8b2bdd963f95440293475611083d9cb929bd6
Atick Faisal <atick.faisal@outlook.com>
2019-02-13 01:12:52 +0600

    added seven segment

---
+++ Seven Segment Display/README.md
@@ -0,0 +1,3 @@
+<h1>Seven Segment Display</h1>
+<h3>Circuit Diagram</h3>
+<img src="seven_segment.png" width="800"/>
---
+++ Seven Segment Display/main.c
@@ -0,0 +1,27 @@
+//-------------------------------------------------//
+//--------------Atick Faisal, 2019-----------------//
+//-------------------------------------------------//
+
+#include "pic16f877a.h"
+
+#define _XTAL_FREQ 16000000
+
+void _init() {
+    TRISC = 0x00;
+    PORTC = 0x00;
+}
+
+void display() {
+    int i;
+    for(i=0; i<10; i++) {
+        PORTC = i;
+        __delay_ms(1000);
+    }
+}
+
+void main(void) {
+    _init();
+    while(1) {
+        display();
+    }
+}
