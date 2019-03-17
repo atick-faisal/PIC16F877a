3b134aa905c844dc6707711dc4d9f374f7506e3e
Atick Faisal <atick.faisal@outlook.com>
2019-03-18 03:36:44 +0600

    added adc

---
+++ Analog to Digital Conv/README.md
@@ -0,0 +1,3 @@
+<h1>ADC - Analog to Digital Conversion</h1>
+<h3>Circuit Diagram</h3>
+<img src="adc.PNG" width="800"/>
---
+++ Analog to Digital Conv/pic16f877a.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   pic16f877a.h
+ * Author: Atick Faisal
+ * License: GPL-3.0
+ * Created on March 18, 2019, 2:28 AM
+ */
+
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#include <xc.h>
