20e0100f067860bd30cbdc84004ae443b01f3554
Atick Faisal <atickfaisal@gmail.com>
2023-10-17 16:27:50 +0300

    Github action test

--- Analog to Digital Conversion/nbproject/Makefile-variables.mk
+++ Analog to Digital Conversion/nbproject/Makefile-variables.mk
@@ -6,8 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=ADC.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/ADC.X.production.hex
-CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=adc.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/adc.x.tar
+CND_ARTIFACT_NAME_default=Analog_to_Digital_Conversion.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Analog_to_Digital_Conversion.production.hex
