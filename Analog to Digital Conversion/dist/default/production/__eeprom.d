a522b9d228b40557b5389349ca8a6a3aec25af0d
Atick Faisal <atickfaisal@gmail.com>
2023-10-18 22:30:06 +0300

    Add gitignore

--- Analog to Digital Conversion/dist/default/production/__eeprom.d
+++
@@ -1,2 +0,0 @@
-dist/default/production/__eeprom.p1: \
-  /opt/microchip/xc8/v2.45/pic/sources/c99/pic/__eeprom.c
--- Timer/dist/default/production/__eeprom.d
+++
@@ -1,2 +0,0 @@
-dist/default/production/__eeprom.p1: \
-  /opt/microchip/xc8/v2.05/pic/sources/c90/pic/__eeprom.c
